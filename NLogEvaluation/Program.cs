1400758ec888a5919dbe00b838a80d6be959f486
michael dudley <michael.dudley@improvingenterprises.com>
2016-03-14 16:32:53 -0500

    nlog database target is working

--- NLogEvaluation/Program.cs
+++ NLogEvaluation/Program.cs
@@ -1,15 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using NLog;
 
 namespace NLogEvaluation
 {
     class Program
     {
+        private static ILogger logger; 
         static void Main(string[] args)
         {
+            logger = LogManager.GetCurrentClassLogger();
+
+            //var eventInfo = new LogEventInfo();
+            //eventInfo.Properties["Foo"] = Guid.NewGuid();
+            //eventInfo.Message = "Foo Message";
+            //eventInfo.Level = LogLevel.Debug;
+            //logger.Log(eventInfo);
+
+            MappedDiagnosticsLogicalContext.Set("messageId", Guid.NewGuid());
+            MappedDiagnosticsLogicalContext.Set("requestId", Guid.NewGuid());
+            MappedDiagnosticsLogicalContext.Set("commandName", "The Command Name");
+
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
         }
     }
 }
